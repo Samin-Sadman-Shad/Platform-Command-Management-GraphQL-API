1c4232f20b1aa9893d0288adee3fe3c93d8336ad
Samin-Sadman-Shad <saminsadmanshad@gmail.com>
2025-06-04 13:11:52 +0600

    platform model added

---
+++ CommanderGQL/Models/Platform.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CommanderGQL.Models
+{
+    public class Platform
+    {
+        [Key]
+        public Guid Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string? License { get; set; }
+    }
+}
