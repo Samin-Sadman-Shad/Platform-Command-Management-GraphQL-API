3150ffe7bf9c98eda1f9704bb2a2ae93a7f6f13b
Samin-Sadman-Shad <saminsadmanshad@gmail.com>
2025-06-06 12:30:44 +0600

    EF core implemented with intial migration

---
+++ CommanderGQL/Persistance/CommanderDbContext.cs
@@ -0,0 +1,15 @@
+﻿using CommanderGQL.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CommanderGQL.Persistance
+{
+    public class CommanderDbContext:DbContext
+    {
+        public CommanderDbContext(DbContextOptions options):base(options)
+        {
+            
+        }
+
+        public DbSet<Platform> Platforms { get; set; }
+    }
+}
--- CommanderGQL/Program.cs
+++ CommanderGQL/Program.cs
@@ -1,5 +1,17 @@
+using CommanderGQL.Persistance;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<CommanderDbContext>(options =>
+            options.UseSqlServer(builder.Configuration.GetConnectionString("CommanderConnectionString")));
+
+
 var app = builder.Build();
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
 
 app.MapGet("/", () => "Hello World!");
 
